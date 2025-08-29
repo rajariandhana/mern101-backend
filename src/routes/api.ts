d97bb9c1e801381c106bb8b830f336e94681d8b3
Ralfazza Rajariandhana <s4949323@student.uq.edu.au>
2025-08-29 15:50:21 +1000

    Middleware Control Access

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
+import { Request, Response } from 'express';
+
 import authController from '../controllers/auth.controller';
 import dummyController from '../controllers/dummy.controller';
 
+import { ROLES } from '../utils/constant';
 import authMiddleware from '../middleware/auth.middleware';
+import aclMiddleware from '../middleware/acl.middleware';
 
 const router = express.Router();
 
@@ -12,5 +16,15 @@ router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
 router.get('/auth/me', authMiddleware, authController.me);
 router.post('/auth/activation', authController.activation);
+
+router.get('/test-acl',
+  [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
+  (req:Request,res:Response) =>{
+    res.status(200).json({
+      data:"success",
+      message:"OK"
+    })
+  }
+);
 
 export default router;
