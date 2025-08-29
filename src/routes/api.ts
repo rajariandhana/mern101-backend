e3f4acf78f4be82c722f03e8e7098b30597cc848
Ralfazza Rajariandhana <s4949323@student.uq.edu.au>
2025-08-29 20:53:43 +1000

    API Category

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -10,6 +10,7 @@ import authMiddleware from '../middleware/auth.middleware';
 import aclMiddleware from '../middleware/acl.middleware';
 import mediaMiddleware from '../middleware/media.middleware';
 import mediaController from '../controllers/media.controller';
+import categoryController from '../controllers/category.controller';
 
 const router = express.Router();
 
@@ -18,6 +19,12 @@ router.post('/auth/register', authController.register);
 router.post('/auth/login', authController.login);
 router.get('/auth/me', authMiddleware, authController.me);
 router.post('/auth/activation', authController.activation);
+
+router.post('/category', [authMiddleware, aclMiddleware([ROLES.ADMIN])], categoryController.create);
+router.get('/category', categoryController.findAll);
+router.get('/category/:id', categoryController.findOne);
+router.put('/category/:id', [authMiddleware, aclMiddleware([ROLES.ADMIN])],categoryController.update);
+router.delete('/category/:id', [authMiddleware, aclMiddleware([ROLES.ADMIN])],categoryController.remove);
 
 router.get('/test-acl',
   [authMiddleware, aclMiddleware([ROLES.ADMIN, ROLES.MEMBER])],
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -10,3 +10,9 @@ export interface IUserToken extends Omit<User,"password"|"activationCode"|"isAct
 export interface IReqUser extends Request {
     user?: IUserToken;
 }
+
+export interface IPaginationQuery {
+  page: number;
+  limit: number;
+  search?: string;
+}
